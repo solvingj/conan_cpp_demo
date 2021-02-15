2338638af6dcbda7a0e09919e6a27c377a890c1d
Jerry Wiltse <jerrywiltse@gmail.com>
2021-02-15 12:59:48 -0500

    fix up hostnames and separate artifactory to separate compose file

--- artifactory/scripts/bootstrap-artifactory-config.sh
+++ artifactory/scripts/bootstrap-artifactory-config.sh
@@ -3,4 +3,4 @@ set -eo pipefail
 
 /entrypoint-artifactory.sh &
 /scripts/wait-for-artifactory.sh
-/scripts/set-internal-hostname.sh &> set-internal-hostname.log
+/scripts/set-local-configuration.sh &> set-local-configuration.log
--- artifactory/scripts/set-local-configuration.sh
+++ artifactory/scripts/set-local-configuration.sh
@@ -6,11 +6,9 @@ curl -m 5 -f -X PATCH \
 -H 'Content-Type: application/yaml' \
 http://localhost:8082/artifactory/api/system/configuration \
 -d '
-urlBase: http://artifactory-conan-demo:8082
+urlBase: http://artifactory-ce-demo:8082
 localRepositories: #Local repositories configuration
-    conan-develop: #The local repository name
-        type: conan #The package type
-    conan-temp: #The local repository name
+    conan-local: #The local repository name
         type: conan #The package type
 '
 # we create the repositories here for the environment
