5b47f8b0b645a5569d5da83b6ae0b028ecaaa967
Jerry Wiltse <jerrywiltse@gmail.com>
2021-02-15 13:01:21 -0500

    match upload scripts to slides

--- examples/upload_package/demo_commands.bat
+++ examples/upload_package/demo_commands.bat
@@ -1,8 +1,8 @@
 conan remote list
-conan remote add artifactory http://artifactory:8081/artifactory/api/conan/conan-local
+conan remote add artifactory  \
+           http://artifactory-ce-demo:8081/artifactory/api/conan/conan-local
 
-conan remote list
-conan upload "regex/0.1.0@demo/demo" -r=artifactory
+conan user -p=password -r=artifactory admin   # Login to Remote
 
-conan search "*" -r=artifactory
-conan search regex/0.1@user/testing -r=artifactory
+conan upload "regex/0.1.0@demo/demo" -r=artifactory --all
+conan search regex/0.1.0@demo/demo -r=artifactory
